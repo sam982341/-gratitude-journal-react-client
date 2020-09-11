4a1cbf479b227ddce94e4c51f0b50a237d0063bf
sam <github9283764@emailturtle.com>
2020-09-11 09:47:56 -0600

    added component and pages folder structure. created navbar.

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from 'react-router-dom/Link';
+
+// MUI Stuff
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+export default function Navbar() {
+	return (
+		<div>
+			<AppBar>
+				<Toolbar className="nav-container">
+					<Button color="inherit" component={Link} to="/">
+						Home
+					</Button>
+					<Button color="inherit" component={Link} to="/login">
+						Login
+					</Button>{' '}
+					<Button color="inherit" component={Link} to="/signup">
+						signup
+					</Button>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
+}
---
+++ src/pages/signup.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class signup extends Component {
+	render() {
+		return <div>Signup Page</div>;
+	}
+}
+
+export default signup;
